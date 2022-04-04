3797af111d986fc545c9b8afba3a91fe1deb76b5
Webdev0627 <86770209+webdev0627@users.noreply.github.com>
2022-04-04 00:53:46 -0400

    Initial commit

---
+++ src/util.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', cleanItAll)
+function cleanItAll() {
+  console.log('triying to clean')
+  document.body.classList.remove('mata-friendly')
+}
+console.log('from util')
